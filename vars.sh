bbda3a17acd0169d608e592dc002ed136b8084cd
tejas-2232 <tejasbachhav98@gmail.com>
2024-02-06 14:09:14 -0500

    add program of using vars

---
+++ vars.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# this is second bash script
+
+echo "What is your name?"
+read name
+echo "what chapter are you on now?"
+read chapter
+
+echo "welcome" $name "to chapter" $chapter
