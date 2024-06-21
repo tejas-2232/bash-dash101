8abb9099d64d4dbb43498cb4104ceabcd0468fad
tejas-2232 <tejasbachhav98@gmail.com>
2024-06-20 23:49:38 -0400

    add system stat check script

---
+++ projects/system_stats/sys_stat.sh
@@ -0,0 +1,38 @@
+#!bin/bash
+#author: tejas bachhav
+#dat: June 20, 2024
+#
+
+echo "********************************SHELL SCRIPTING******************************"
+
+sleep 2
+echo "Hey There, so you want to check your system stats?"
+
+sleep 3
+
+echo "Please Enter your Name"
+read name
+
+echo "hi $name , welcome"
+
+echo "***************************************************************"
+echo "                                                               "
+
+date | awk ''
+
+echo "******************** Current Date and Time ***********************"
+
+echo "******************** Disk space Available ***********************"
+
+echo "******************** User Uptime ***********************"
+
+echo "******************** Last 3 Login Details ***********************"
+
+echo "******************** currently connected ***********************"
+
+
+
+
+
+
+
