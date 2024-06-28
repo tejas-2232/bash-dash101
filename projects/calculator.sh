dbdc1e2861fde1568c809ed1b60937ed54bff6da
tejas-2232 <tejasbachhav98@gmail.com>
2024-06-27 23:56:31 -0400

    file add

---
+++ projects/calculator.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+#
+
+echo "Select operation:"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read -p "Enter your choice (1-4): " choice
+
+# Prompt user to enter two numbers
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+
+
