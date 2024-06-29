505e3310511b418b113588ec467000a7b7d62684
tejas-2232 <tejasbachhav98@gmail.com>
2024-06-29 18:07:35 -0400

    file add

--- projects/calculator.sh
+++ projects/calculator.sh
@@ -12,4 +12,24 @@ read -p "Enter your choice (1-4): " choice
 read -p "Enter first number: " num1
 read -p "Enter second number: " num2
 
+if [ "$choice" -eq 1]; then
+	result=$(echo "$num1 + $num2" | bc)
+	echo "Result : $result"
+elif [ "$choice" -eq 2]; then
+	result=$(echo "$num1 - $num2" | bc)
+	echo "Result : $result"
 
+elif [ "$choice" -eq 3]; then
+	result=$(echo "$num1 * $num2" | bc)
+	echo "Result : $result"
+
+elif [ "$choice" -eq 4]; then
+	if [ "$num2" -eq 0]; then
+		echo "Error: can't divide the number by 0"
+	else
+		result=$(echo "$num1 / $num2" | bc)
+		echo "Result : $result"
+	fi
+else
+	echo "Invalid choice, select valid operation"
+fi
